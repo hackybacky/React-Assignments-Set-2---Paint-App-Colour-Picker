3c87f7f277ae56cdf4b766620a1d3523b3576089
hackybacky <hacky3244@gmail.com>
2022-04-08 22:29:33 +0530

    added details

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React, { useState, useEffect } from "react";
+import "./../styles/Child.css";
 
+export default function Selection(props) {
+  const { applyColor } = props;
+  const [background, setBackground] = useState({ background: "" });
+  useEffect(() => {
+    console.log("here", background);
+  }, [background]);
+  return (
+    <div
+      className="fix-box"
+      style={background}
+      onClick={() => applyColor(setBackground)}
+    >
+      <h2 className="subheading"> Selection </h2>
+    </div>
+  );
+}
