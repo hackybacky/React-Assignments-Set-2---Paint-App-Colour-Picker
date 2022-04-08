2919d8b12e3900d6703f5eec53e9dcb7d30fc56c
hackybacky <hacky3244@gmail.com>
2022-04-08 22:34:15 +0530

    add bg color to button

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props;
   const { background } = config;
   return (
     <button
       className={config.className}
+      style={{ background }}
       onClick={() => selectNextBackground({ background: background })}
     >
       {config.label}
